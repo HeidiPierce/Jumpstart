6e6cd1af574abc35848d41595cb85107cd3f3110
HeidiPierce <dotyh1969@yahoo.com>
2017-10-29 23:52:45 -0700

    final commit---completed carousel and hiding nav, but there are spacing issues and size of img's is off. Ran out of time to fix before class ends.

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.carousel').slick({
+  $('.single-item').slick({
     autoplay: true,
     autoplaySpeed: 4000,
     arrows: false,
@@ -8,20 +8,19 @@ $(document).ready(function() {
     draggable: false,
   });
 
+  var userScroll = 0;
+    $(window).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop - userScroll >= 50) {
+        var heightNavbar = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + heightNavbar}, 150);
+        userScroll = scrollTop;
+      } else if (userScroll - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        userScroll = scrollTop;
+      }
+    });
 
-  var lastScrollTop = 0;
- +  $(window).scroll(function(){
- +    var scrollTop = $(this).scrollTop();
- +    if (scrollTop - lastScrollTop > 50){
- +      var navHeight = $('.navbar').css('height');
- +      $('.navbar').animate({top: '-' + navHeight}, 150);
- +      lastScrollTop = scrollTop;
- +    } else if (lastScrollTop - scrollTop > 50) {
- +      $('.navbar').animate({top: '0px'}, 150);
- +      lastScrollTop = scrollTop;
- +    }
- +  });
-  
 
 
 });
